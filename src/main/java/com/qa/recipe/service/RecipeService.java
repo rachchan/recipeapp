556d9ccc5147a93a16e2ef061f270c7dcbc0e7ae
Rachel Chan <rachelchan1497@gmail.com>
2020-06-25 17:23:36 +0100

    updated service

--- src/main/java/com/qa/recipe/service/RecipeService.java
+++ src/main/java/com/qa/recipe/service/RecipeService.java
@@ -33,14 +33,34 @@ public class RecipeService {
 	public Recipe getRecipe(String name) {
 		return repo.findRecipeByName(name).orElseThrow(() -> new NotFoundException());
 	}
+	
 	/**
 	 * Inserts recipes into the database
 	 */
 	public Recipe create (Recipe recipe) {
-		this.repo.save(recipe);
-		return null;
+		return this.repo.save(recipe);	
 	}
 	
+	/**
+	 *Updates recipes in database
+	 */
+	public Recipe update(Recipe recipe, long id) {
+		Recipe toUpdate = this.repo.findById(id).orElseThrow(() -> new NotFoundException());
+		
+		toUpdate.setIngredients(recipe.getIngredients());
+		toUpdate.setName(recipe.getName());
+		toUpdate.setTime(recipe.getTime());
+		
+		return this.repo.save(toUpdate);
+	}
+	
+	/**
+	 * Deletes recipe from id entered
+	 */
+	public boolean delete (Long id) {
+		this.repo.deleteById(id);
+		return this.repo.existsById(id);
+	}
 	
 	
 }
