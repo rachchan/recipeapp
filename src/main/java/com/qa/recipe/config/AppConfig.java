4e698b23201258bf065bd846f450111d4a432ea1
Rachel Chan <rachelchan1497@gmail.com>
2020-06-26 11:21:58 +0100

    added bean

---
+++ src/main/java/com/qa/recipe/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.qa.recipe.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.ModelMap;
+
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public ModelMap mapper() {
+		return new ModelMap();
+	}
+}
--- src/main/java/com/qa/recipe/persistence/entity/Recipe.java
+++ src/main/java/com/qa/recipe/persistence/entity/Recipe.java
@@ -63,6 +63,53 @@ public class Recipe {
 		public void setTime(Double time) {
 			this.time = time;
 		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			result = prime * result + ((ingredients == null) ? 0 : ingredients.hashCode());
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			long temp;
+			temp = Double.doubleToLongBits(time);
+			result = prime * result + (int) (temp ^ (temp >>> 32));
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Recipe other = (Recipe) obj;
+			if (id == null) {
+				if (other.id != null)
+					return false;
+			} else if (!id.equals(other.id))
+				return false;
+			if (ingredients == null) {
+				if (other.ingredients != null)
+					return false;
+			} else if (!ingredients.equals(other.ingredients))
+				return false;
+			if (name == null) {
+				if (other.name != null)
+					return false;
+			} else if (!name.equals(other.name))
+				return false;
+			if (Double.doubleToLongBits(time) != Double.doubleToLongBits(other.time))
+				return false;
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "Recipe [id=" + id + ", name=" + name + ", ingredients=" + ingredients + ", time=" + time + "]";
+		}
 		
 		
 }
